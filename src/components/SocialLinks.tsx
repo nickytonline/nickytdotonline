dccec92c3c9a7cca6ededcc488e7aefd3a30a268
Nick Taylor <nick@nickyt.co>
2024-12-03 08:23:33 -0500

    reworked links and added a header

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -4,10 +4,12 @@ import { socialLinks } from '../constants/socialLinks';
 
 export const SocialLinks: React.FC = () => {
   return (
-    <ul className="flex flex-wrap justify-center gap-4">
-      {socialLinks.map((link) => (
-        <SocialLink key={link.href} {...link} />
-      ))}
-    </ul>
+    <nav aria-label="social links">
+      <ul className="flex flex-wrap justify-center gap-4">
+        {socialLinks.map((link) => (
+          <SocialLink key={link.href} {...link} />
+        ))}
+      </ul>
+    </nav>
   );
 };
