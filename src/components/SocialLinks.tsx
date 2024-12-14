6c16c5b0a4eb6c421fd6dfca4b7e30132256f0e7
Nick Taylor <nick@nickyt.co>
2024-12-14 09:07:44 -0500

    fix: small layout fix for smaller screens

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -5,7 +5,7 @@ import { socialLinks } from '../constants/socialLinks';
 export const SocialLinks: React.FC = () => {
   return (
     <nav aria-label="social links">
-      <ul className="flex flex-wrap justify-center gap-4">
+      <ul className="grid grid-cols-4 md:grid-cols-8 justify-center gap-4">
         {socialLinks.map((link) => (
           <SocialLink key={link.href} {...link} />
         ))}
